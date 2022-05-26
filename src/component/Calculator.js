ca78a5c2e5d844400abcbf0d2fd79777bd26f91a
Assad Isah <nottherealalanturing@gmail.com>
2022-05-26 14:43:14 +0100

    feat: link the calculate function to UI

--- src/component/Calculator.js
+++ src/component/Calculator.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './styles/Calculator.css';
+import calculate from '../logic/calculate';
 
 const buttonsArray = [
   'AC',
@@ -28,18 +29,42 @@ const operatorsArray = ['÷', '*', '-', '+', '='];
 class Calculator extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      total: 0,
+      next: null,
+      operation: null,
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(e) {
+    this.setState((state) => ({
+      ...state,
+      ...calculate(state, e.target.name),
+    }));
   }
 
   render() {
+    const { total, next, operation } = this.state;
+
     return (
       <div className="calculatorBody">
-        <input type="text" className="result" value={0} />
+        <p type="text" className="result">
+          {total}
+          {operation}
+          {next}
+        </p>
         <div className="calculatorButtons">
           {buttonsArray.map((value) => {
             const operator = operatorsArray.includes(value) ? 'operator' : '';
             return (
-              <button className={`calculatorBtn ${operator}`} type="button" key={value}>
+              <button
+                className={`calculatorBtn ${operator}`}
+                type="button"
+                key={value}
+                name={value}
+                onClick={this.handleClick}
+              >
                 {value}
               </button>
             );
